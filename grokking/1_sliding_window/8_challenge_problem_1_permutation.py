a5c9db4227ea76583271e20ee8f68db263b06477
Schartz Rehan <rehan.qt@gmail.com>
2022-07-04 16:59:40 +0530

    add proper solution for challenge problem 1

--- grokking/1_sliding_window/8_challenge_problem_1_permutation.py
+++ grokking/1_sliding_window/8_challenge_problem_1_permutation.py
@@ -15,52 +15,64 @@ def print_execution_time(func, *args, **kwargs):
 
 @print_execution_time
 def has_permutation(str, pattern):
-	start = 0
-	occurance_count = 0
-	flag = False
+    start = 0
+    occurance_count = 0
+    flag = False
 
-	for end in range(len(str)):
-		right_char = str[end]
+    for end in range(len(str)):
+        right_char = str[end]
 
-		if right_char in pattern:
-			flag = True
-			occurance_count += 1
-		else:
-			flag = False
-			occurance_count =0
-			
+        if right_char in pattern:
+            flag = True
+            occurance_count += 1
+        else:
+            flag = False
+            occurance_count =0
+            
 
-		if occurance_count == len(pattern):
-			return True
+        if occurance_count == len(pattern):
+            return True
 
-	return False
+    return False
 
 
-@print_execution_time
+
 def has_permutation_correct(str, pattern):
-	start = 0
-	occurance_count = 0
-	flag = False
+    frequency = {}
+    start = 0
+    matched = 0
+    for c in pattern:
+        if c not in frequency:
+            frequency[c] = 0
+        frequency[c] += 1
 
-	for end in range(len(str)):
-		right_char = str[end]
+    for end in range(len(str)):
+        right_char = str[end]
+        if right_char in frequency:
+            frequency[right_char] -= 1
 
-		if right_char in pattern:
-			flag = True
-			occurance_count += 1
-		else:
-			flag = False
-			occurance_count =0
-			
+            if frequency[right_char] == 0:
+                matched += 1
+        while(end - start + 1) > len(pattern):
+            left_char = str[start]
 
-		if occurance_count == len(pattern):
-			return True
-
-	return False
-
-
+            if left_char in frequency:
+                if frequency[left_char] == 0:
+                    matched -= 1
+                frequency[left_char] += 1
+            start += 1
+        if matched == len(frequency):
+            return True
+    return False
 
 print(has_permutation("oidbcaf", "abc"))
 print(has_permutation("odicf", "dc"))
 print(has_permutation("bcdxabcdy", "bcdyabcdx"))
 print(has_permutation("aaacb", "abc"))
+
+print("======================================")
+
+print(has_permutation_correct("oidbaaf", "abc"))
+print(has_permutation_correct("odicf", "dc"))
+print(has_permutation_correct("bcdxabcdy", "bcdyabcdx"))
+print(has_permutation_correct("aaacb", "abc"))
